0cccf24224ca9b05d85a553a7a65761c3c2a1030
Kogun <kaycop6@gmail.com>
2026-02-28 13:49:17 -0600

    Add files via upload

---
+++ cheesehacks2/cheesehacks2/cheesehacks2App.swift
@@ -0,0 +1,17 @@
+//
+//  cheesehacks2App.swift
+//  cheesehacks2
+//
+//  Created by Kevin Chon on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct cheesehacks2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cheesehacks2/cheesehacks2Tests/cheesehacks2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  cheesehacks2Tests.swift
+//  cheesehacks2Tests
+//
+//  Created by Kevin Chon on 2/28/26.
+//
+
+import Testing
+@testable import cheesehacks2
+
+struct cheesehacks2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
